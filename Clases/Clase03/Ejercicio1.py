1f19e3e7ff98c832e2e8aa9aa2351c02c0501dc9
Juanba303 <ljficco@gmail.com>
2026-06-04 00:52:48 +0000

    Ejercicios de la tercera clase hechos

---
+++ Clases/Clase03/Ejercicio1.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingrese su número: "))
+suma = 0
+
+for i in range(numero):
+    i += 1
+    suma += i
+
+print(suma)
---
+++ Clases/Clase03/Ejercicio3.py
@@ -0,0 +1,18 @@
+numero = 303
+respuesta = 0
+intentos = 0
+
+while True:
+    respuesta = input("Ingresá un número: ")
+    
+    while type(respuesta) != type(1):
+        respuesta = input("No ingresaste un numero. Ingresá un número: ")
+    if respuesta > numero:
+        print("Es más chico")
+        intentos += 1
+    elif respuesta < numero:
+        print("Es más grande")
+        intentos += 1
+    else:
+        print(f"¡Adivinaste! Te tomó {intentos} intentos")
+        break
