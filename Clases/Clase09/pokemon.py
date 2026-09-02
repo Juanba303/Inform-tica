26e05045e5e2b34b112e3b381655272ad678d50b
Juanba303 <ljficco@gmail.com>
2026-09-02 20:37:24 +0000

    Clase 9

--- Clases/Clase09/pokemon.py
+++ Clases/Clase09/pokemon.py
@@ -8,10 +8,33 @@ def consultar_api(url):
     except requests.exceptions.RequestException as e:
         print("Error al consultar la API:", e)
         return None                 
-        
 
-url = "https://pokeapi.co/api/v2/pokemon/pikachu" 
-datos = consultar_api(url)
+peso_promedio = 0
+nombres = ["charmeleon", "squirtle", "charmander", "jigglypuff", "alakazam"]
+pokemones = []
+for nombre in nombres:
+    url = f"https://pokeapi.co/api/v2/pokemon/{nombre}" 
+    datos = consultar_api(url)
+    if datos is not None:
+        pokemones.append(datos)
 
-if datos is not None:
-    print(datos["name"], "pesa", datos["weight"])
+resumen = []
+for p in pokemones:
+    resumen.append({
+        "nombre": p["name"],
+        "tipo":   p["types"][0]["type"]["name"],
+        "peso":   p["weight"],
+    })
+
+print(f"{'NOMBRE':<12}{'TIPO':<10}{'PESO(kg)':>9}")
+print("-" * 31)
+for p in resumen:
+    print(f"{p['nombre']:<12}{p['tipo']:<10}{p['peso']/10:>9.1f}")
+    peso_promedio += p["peso"]/10
+
+print("\nSolo los tipo fuego:")
+for p in resumen:
+    if p["tipo"] == "fire":
+        print(p["nombre"])
+
+print(f"Peso promedio: {peso_promedio / len(pokemones)}")
